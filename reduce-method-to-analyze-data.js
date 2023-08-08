a0f5a9ad72ebcb6112240872480d1a090caa2275
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-08 06:44:10 -0400

    Save reduce-method-to-analyze-data.js.

---
+++ reduce-method-to-analyze-data.js
@@ -0,0 +1,9 @@
+//recreate an array the will hold a list of IMDB rating which you will use later of find the average ratting on the movies
+
+//iterate over the watchList Array
+
+//find if the person who directed is Christopher Nolan 
+
+// if it is not move on to thr next one
+
+// if it is 
