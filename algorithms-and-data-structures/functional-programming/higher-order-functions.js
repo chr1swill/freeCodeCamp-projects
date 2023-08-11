980385544f03b697d61ed1daeb2283615b1b97a1
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-11 13:00:18 -0400

    Save higher-order-functions.js.

---
+++ algorithms-and-data-structures/functional-programming/higher-order-functions.js
@@ -0,0 +1,3 @@
+// take the array and filter out the negative numbers and the numbers numbers that are not whole
+
+// take that new array and map through the elements using reducs to return the square of every number as a new arr 
