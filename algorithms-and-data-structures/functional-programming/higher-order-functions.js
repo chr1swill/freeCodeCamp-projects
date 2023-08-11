ec4951b5885419ca8f5ee37a5b7eb5ba373dff70
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-11 19:39:11 -0400

    Save higher-order-functions.js.

--- algorithms-and-data-structures/functional-programming/higher-order-functions.js
+++ algorithms-and-data-structures/functional-programming/higher-order-functions.js
@@ -1,3 +1,5 @@
 // take the array and filter out the negative numbers and the numbers numbers that are not whole
 
 // take that new array and map through the elements using reducs to return the square of every number as a new arr 
+
+const squareList = arr =
