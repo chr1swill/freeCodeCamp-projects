ddcf1a854b7dde3de621176c3c664f1263f4c597
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-11 20:01:33 -0400

    Save higher-order-functions.js.

--- algorithms-and-data-structures/functional-programming/higher-order-functions.js
+++ algorithms-and-data-structures/functional-programming/higher-order-functions.js
@@ -3,7 +3,14 @@
 // take that new array and map through the elements using reducs to return the square of every number as a new arr 
 
 const squareList = arr => {
-    arr.filter((element) => 
-        return element >= 0 && Number.isInteger(element)
-    )
+    
+   const filteredArray =  arr.filter((element) => {
+        return element >= 0 && Number.isInteger(element);
+    });
+    
+    const squaredNumbers = filteredArray.map((num) => {
+        return Math.pow(num, 2);
+    });
+    
+    return squaredNumbers;
 }
