0e9d409b19b512bcc882b79435b1effa8ffd9aaa
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-16 06:16:45 -0400

    Save squaredList.js.

---
+++ algorithms-and-data-structures/functional-programming/squaredList.js
@@ -0,0 +1,9 @@
+// iterate through array and check if number is positive 
+
+// if the number is positive keep it ig not on to the next 
+
+// take that new array and create another array with thr left over number to the power of 2 
+
+const squaredList = arr => {
+    const positiveIntegers = arr.filter((index) Number.isInteger(index) && index > 0)
+    consol
