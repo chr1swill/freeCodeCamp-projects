726f54f6e3349d4fb78d95a5e65583f993818848
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-22 19:56:18 -0400

    Save convert-string-to-url-slugs.js.

---
+++ algorithms-and-data-structures/functional-programming/convert-string-to-url-slugs.js
@@ -0,0 +1,13 @@
+// set string to lower cases 
+//split string by " "
+// loop through array 
+// if you on thr last element of the array just add the element 
+// if not on the last one add the index + "-" to array
+// take that new array and run join.("")
+
+const title = "A Mind Needs Books Like A Sword Needs A Whetstone";
+
+
+const setToLowerCase = (string) => {
+   return string.toLowerCase()
+}
