60b08200601cf28263705469ccc5f428bafb8f75
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-23 00:27:41 -0400

    feat - add setToLowerCase(title) to const

--- algorithms-and-data-structures/functional-programming/convert-string-to-url-slugs.js
+++ algorithms-and-data-structures/functional-programming/convert-string-to-url-slugs.js
@@ -1,13 +1,14 @@
-// set string to lower cases 
+// set string to lower cases
 //split string by " "
-// loop through array 
-// if you on thr last element of the array just add the element 
+// loop through array
+// if you on thr last element of the array just add the element
 // if not on the last one add the index + "-" to array
 // take that new array and run join.("")
 
 const title = "A Mind Needs Books Like A Sword Needs A Whetstone";
 
-
 const setToLowerCase = (string) => {
-   return string.toLowerCase()
-}
+  return string.toLowerCase();
+};
+
+const titleToLowerCase = setToLowerCase(title);
