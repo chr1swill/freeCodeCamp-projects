a1a12f281934d4dc6260e99fdf11e1919909b3e6
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-25 12:44:31 -0400

    Save sum-of-range.js.

--- algorithms-and-data-structures/intermediate-algorithm-scripting/sum-of-range.js
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/sum-of-range.js
@@ -1,7 +1,16 @@
 //* return the sum of all numbers in a range 
 //* take two params thses are not order dependant and the logic will interperate the lesser of the two number to be the first and other to be second before running the script
 
+// create areay to hold numbers 
+// figure out which number is greatest and which is least of params
 // find all the whole numbers between two params 
 // add those number to an array
 // reduce that array to get the sum 
+let numbers = []
 
+const lowNumber = Math.min(x, y):
+const highNumber = Math.max(x,y);
+
+for (let i = lowNumber; i <= highNumber; i++) {
+  numbers.push(i)  
+}
