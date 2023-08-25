e077304be1a5f465df1b7b3b302eb63f551fc85d
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-25 06:41:59 -0400

    Save sum-of-range.js.

---
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/sum-of-range.js
@@ -0,0 +1,3 @@
+//* return the sum of all numbers in a range 
+//* take two params thses are not order dependant and the logic will interperate the lesser of the two number to be the first and other to be second before running the script
+
