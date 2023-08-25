8cb64e454d0784f33c7614ce02548f414d86b395
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-25 12:23:57 -0400

    Save sum-of-range.js.

--- algorithms-and-data-structures/intermediate-algorithm-scripting/sum-of-range.js
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/sum-of-range.js
@@ -2,3 +2,5 @@
 //* take two params thses are not order dependant and the logic will interperate the lesser of the two number to be the first and other to be second before running the script
 
 // find all the whole numbers between two params 
+// add those number to an array
+// reduce that array to get the sum 
