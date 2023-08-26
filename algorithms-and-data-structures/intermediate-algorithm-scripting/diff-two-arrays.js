d0d86985299cd1cf03b74663ffe1e81bc27edcac
Chris <127428764+chr1swill@users.noreply.github.com>
2023-08-26 10:34:35 +0000

    fix - fix spelling error

--- algorithms-and-data-structures/intermediate-algorithm-scripting/diff-two-arrays.js
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/diff-two-arrays.js
@@ -1,9 +1,9 @@
 // return an array of all the element that are not in both array 
 //* take two arrays arrays as params
 
-// create empty arry to hold all the different items 
+// create empty array to hold all the different items 
 //starting with array1 loop though it 
 // for each index check if in array2
 //if its not push the index to different items array 
 // do the same thing checking arry2 against array1 
-//return different items array 
+// return different items array 
