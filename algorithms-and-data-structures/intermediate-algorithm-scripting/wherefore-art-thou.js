4d1b19d4ff31030684cbcee7f8e4441f2d5c7b3c
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-09-06 18:29:11 -0400

    feat - add step to solution

---
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/wherefore-art-thou.js
@@ -0,0 +1,6 @@
+// *** look through array of objects return object with matching key and value pair ***
+// loop through array of objects
+// check if key and value match
+// if not matching continue
+// if match return object
+//else return no matching object 
