bab23df687a2ad5fa89f2e6891835c19e1cdcba1
chr1swill <alexanderwilliams20003@gmail.com>
2023-09-10 13:01:05 -0400

    feat - add the steps

---
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
@@ -0,0 +1,6 @@
+// split string by capital letters or "-" or "_"
+// set that that the array to lowercase using map
+// loop through the array
+// if it is the last element of the array do nothing
+// else current index + "-"
+// join the array back together
