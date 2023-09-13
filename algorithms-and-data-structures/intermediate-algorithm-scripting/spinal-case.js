4c28eaf0489e19f7d5beb4f258ca1f0ae70ea8ff
Chris <127428764+chr1swill@users.noreply.github.com>
2023-09-13 16:27:31 +0000

    feat - add tests

--- algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
@@ -4,3 +4,10 @@
 // if it is the last element of the array do nothing
 // else current index + "-"
 // join the array back together
+
+const test1 = "This Is Spinal Tap"
+const test2 = "thisIsSpinalTap"
+const test3 = "The_Andy_Griffith_Show"
+const test4 = "Teletubbies say Eh-oh"
+const test5 = "AllThe-small Things"
+
