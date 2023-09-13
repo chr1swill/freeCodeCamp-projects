7e92cbd72ae4104bece37cf24140a39b8874e606
Chris <127428764+chr1swill@users.noreply.github.com>
2023-09-13 13:42:25 +0000

    refactor - change solution logic

--- algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
@@ -1,6 +1,6 @@
 // split string by capital letters or "-" or "_"
-// set that that the array to lowercase using map
-// loop through the array
+//loop through array 
+// set that that the item to lowercase
 // if it is the last element of the array do nothing
 // else current index + "-"
 // join the array back together
