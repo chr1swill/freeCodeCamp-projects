6b74a214cab6b08b54ba14766503d1298656ff5a
Chris <127428764+chr1swill@users.noreply.github.com>
2023-09-13 23:27:04 +0000

    feat - add logic to split string and trim extra space

--- algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
+++ algorithms-and-data-structures/intermediate-algorithm-scripting/spinal-case.js
@@ -11,3 +11,8 @@ const test3 = "The_Andy_Griffith_Show"
 const test4 = "Teletubbies say Eh-oh"
 const test5 = "AllThe-small Things"
 
+const splitWords = (string) => string.split(/(?=[A-Z])|[-_]/);
+
+const trimSpace = (array) => array.map((element) => element.trim())
+
+
